da85c924ba4a5e1ee71554faf371f9716460f75f
Bükki Dániel <dbukki@freemail.hu>
2024-04-08 22:54:03 +0200

    Modification of the compare command script to perform accumulation.

--- scripts/analyze/compare.sh
+++ scripts/analyze/compare.sh
@@ -3,11 +3,40 @@
 # Parameters
 cur_path=$(readlink -f "$0")
 cur_dir=$(dirname "${cur_path}")
-left="$1"; shift
-right="$1"; shift
+dir="$1"; shift
 
 # Constants
 cmp_exe="${cur_dir}/../../build/ccexcmp"
+acc_exe="${cur_dir}/../../build/ccexacc"
+cmp_list="cc-compare.txt"
+cmp_ext=".log"
+res_file="cc-results.txt"
 
-# Compare
-"${cmp_exe}" "${left}" "${right}" | tee "${left}-${right}.log"
+# Ensure existence of compare list
+cd "${dir}"
+if ! [ -f "${cmp_list}" ]; then
+    list=""
+    while read tag; do
+        list="${list}${tag%/}\n"
+    done < <(ls -d */)
+    echo -e "${list}" > "${cmp_list}"
+    echo "Tag compare list has been exported to ${cmp_list}."
+    echo "Make adjustments to the list now as you wish, then press ENTER to begin comparison."
+    read
+fi
+
+# Compare consecutive tags in the list
+prev=""
+while read next; do
+    if ! [ -z "${next}" ]; then
+        if ! [ -z "${prev}" ]; then
+            echo -e "\n --- Comparing \"${prev}\" with \"${next}\"  ---\n"
+            "${cmp_exe}" "${prev}" "${next}" | tee "${prev}-${next}${cmp_ext}"
+        fi
+        prev="${next}"
+    fi
+done < "${cmp_list}"
+
+# Accumulate results of comparisons
+echo -e "\n --- Accumulating results into \"${res_file}\" ---\n"
+"${acc_exe}" . "${cmp_ext}" | tee "${res_file}"
